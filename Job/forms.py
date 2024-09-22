ac8a2981142cafdb59edf0199fdfd311930a830b
auob123 <amjedelsadig@gmail.com>
2024-09-22 18:13:58 +0500

    making now django app

---
+++ Job/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from Job.models import *
+import json
+import os
+from collegegig.settings import MEDIA_ROOT
+from taggit.forms import TagWidget
+
+
---
+++ Job/urls.py
@@ -0,0 +1,2 @@
+from django.urls import path
+from Job import views
