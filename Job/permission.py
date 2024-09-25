432b8e6d8e1f980f1d01199cc67b5a9f3d49aee6
auob123 <amjedelsadig@gmail.com>
2024-09-25 22:15:22 +0500

    ..

--- Job/permission.py
+++ Job/permission.py
@@ -1,2 +1,24 @@
 from django.core.exceptions import PermissionDenied
 
+
+
+def user_is_student(function):
+    def wrap(request, *args, **kwargs):
+
+        if request.user.role == 'student':
+            return function(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+    return wrap
+
+
+def user_is_faculty(function):
+    def wrap(request, *args, **kwargs):
+
+        if request.user.role == 'faculty':
+            return function(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+    return wrap
